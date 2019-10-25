93253e45b26bdbc09f8bd673cad78e65ad67f087
tada3 <tada3@outlook.com>
2019-10-25 22:21:07 +0900

    to 20.

--- oioclient.sh
+++ oioclient.sh
@@ -3,6 +3,6 @@
 # java -cp build/libs/netty-test.jar netty.test.client.OIOEchoClient
 #java -cp build/libs/netty-test.jar netty.test.client.OIOEchoClient localhost 9090 10 600 10
 
-java -cp build/libs/netty-test.jar netty.test.client.OIOEchoClient yoshihara-test001-clv-jp2v-dev.lineinfra-dev.com 9090 10 600 10
+java -cp build/libs/netty-test.jar netty.test.client.OIOEchoClient yoshihara-test001-clv-jp2v-dev.lineinfra-dev.com 9090 10 600 20
 
 
